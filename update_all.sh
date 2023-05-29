66089ea5700d4ced1137703bda2e0b35cbcdb44b
Junelle-Matthias <86528671+Junelle-Matthias@users.noreply.github.com>
2023-05-29 10:54:30 -0400

    Update update_all.sh

--- update_all.sh
+++ update_all.sh
@@ -10,14 +10,15 @@ echo "Trying $f3 and tsv/$f3\_orgs.tsv"
 
 ddhi_update_orgs updated/$f3\_pers.xml tsv/$f3\_orgs.tsv > updated/$f3\_orgs.xml
 
-echo "Trying $f3 and tsv/$f3\_places.tsv"
-
-ddhi_update_places updated/$f3\_orgs.xml tsv/$f3\_places.tsv > updated/$f3\_places.xml
-
 echo "Trying $f3 and tsv/$f3\_events.tsv"
 
 ddhi_update_events updated/$f3\_places.xml tsv/$f3\_events.tsv > updated/$f3\_updated.tei.xml
 
+echo "Trying $f3 and tsv/$f3\_places.tsv"
+
+ddhi_update_places updated/$f3\_orgs.xml tsv/$f3\_places.tsv > updated/$f3\_places.xml
+
+
 rm updated/$f3\_pers.xml
 rm updated/$f3\_orgs.xml
-rm updated/$f3\_places.xml
+rm updated/$f3\_events.xml
